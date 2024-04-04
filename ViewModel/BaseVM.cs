13aa59aa4099f7be720ebedfcd97a63c3808882b
Student <Student@CAB4-10>
2024-04-04 11:21:27 +1000

    Добавьте файлы проекта.

---
+++ ViewModel/BaseVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sanatory.ViewModel
+{
+        public class BaseVM : INotifyPropertyChanged
+        {
+            protected void Signal([CallerMemberName] string caller = null)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(caller));
+            }
+            public event PropertyChangedEventHandler? PropertyChanged;
+
+
+        }
+    
+}
---
+++ ViewModel/MainWindowVM.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Sanatory.ViewModel
+{
+    public class MainWindowVM : BaseVM
+    {
+        private Page currentPage;
+
+        public Page CurrentPage { get => currentPage;
+        set
+            {
+                currentPage = value;
+                Signal();
+            }
+        }
+
+        public CommandVM Pages {  get; set; }
+
+        public MainWindowVM() 
+        {
+            Pages = new CommandVM(() =>
+            {
+                OpenPages();
+            });
+            
+            
+
+        }
+
+    }
+}
